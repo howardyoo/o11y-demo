fb3661185a743de669d2e9ef069cb1a264a34f02
gkspranger <greg@spranger.us>
2024-03-12 12:52:36 -0400

    ok, all ready and based out

--- cons/app.py
+++ cons/app.py
@@ -25,25 +25,49 @@ DB = connect(host="db",
             )
 
 def logit(msg):
+    """making sure we are printing out to stderr
+    so log can be seen while running docker-compose"""
     print(f"CONSQUE: {msg}", file=sys.stderr)
 
-def query_db():
-    logit(f"this is just to note i have done something against the DB")
-    logit("queried DB")
-    with DB.cursor() as cursor:
-        cursor.execute("SELECT * FROM test")
-        cursor.fetchall()
-
-def on_message(chan, method_frame, header_frame, body, userdata=None):
-    logit(f"this is an event log just to show something important was processed")
+def go_slow_p():
+    """return boolean on whether or something should go slow"""
     num = random.random()
     new_num = num * 100
-    if new_num > 95:
-        time.sleep(1.5)
+    return new_num > 95
+
+def pure_db_proc(rows):
+    """this is attempting to show use of a pure function that
+    should not be polluted with side effects"""
+    return rows + ["some other thing"]
+
+def query_db():
+    """this is some imperative shell that is calling some DB and then
+    is being processed by some pure function that returning that"""
+    logit(f"this is just to note i have done something against the DB")
+    logit("queried DB")
+    result = []
+    with DB.cursor() as cursor:
+        cursor.execute("SELECT * FROM test")
+        result = cursor.fetchall()
+    return pure_db_proc(result)
+
+def pure_message_proc(body):
+    """this is attempting to show use of a pure function that
+    should not be polluted with side effects"""
+    return f"{body}-plus-some-message-proc"
+
+def on_message(chan, method_frame, header_frame, body, userdata=None):
+    """this is the main function that will handle queue messages
+    and do some business logic on it"""
+    logit(f"this is an event log just to show something important was processed")
+    time.sleep(1.5) if go_slow_p() else False
     chan.basic_ack(delivery_tag=method_frame.delivery_tag)
     query_db()
+    return pure_message_proc(body)
 
 def cons_queue():
+    """this is the main function that hooks up the queue and passes off
+    to the handler any messages received"""
     logit(f"called consumer setup queue function")
     on_message_callback = functools.partial(
          on_message,
